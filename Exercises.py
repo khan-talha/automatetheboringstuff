fc3411a096eb853a5a1843b664d7c679996bdd41
Talha Nauman Ahmad Khan <53531107+chewsdata@users.noreply.github.com>
2021-05-11 03:10:42 +0200

    Python practice of programs

---
+++ Exercises.py
@@ -0,0 +1,14 @@
+#This program says hello and asks for my name
+
+print('Hello World')
+
+print('What is your name?') #Ask for their name
+myName = input()
+print('It is nice to meet you, ' + myName)
+print('The length of your name is:')
+print(len(myName))
+
+print('Whats your age:') #Ask for age
+myAge = input()
+print('You have orbited the sun ' + str(int(myAge)) + ' times till now.')
+
